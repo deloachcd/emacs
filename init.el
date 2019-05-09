6d350200857e11ce81e84ff849ff51d48aa1eb9d
Chandler DeLoach <chandler.deloach@micfo.com>
2019-05-09 09:37:05 -0400

    Adding support for different fonts between my iMac and my personal linux laptop.

--- init.el
+++ init.el
@@ -134,11 +134,18 @@ values."
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
-   dotspacemacs-default-font '("Source Code Variable"
-                               :size 14
-                               :weight normal
-                               :width normal
-                               :powerline-scale 1.1)
+   dotspacemacs-default-font (if (eq system-type 'darwin)
+                                 '("Source Code Pro"
+                                   :size 14
+                                   :weight normal
+                                   :width normal
+                                   :powerline-scale 1.1)
+                               '("Source Code Variable"
+                                 :size 14
+                                 :weight normal
+                                 :width normal
+                                 :powerline-scale 1.1))
+
    ;; The leader key
    dotspacemacs-leader-key "SPC"
    ;; The key used for Emacs commands (M-x) (after pressing on the leader key).
