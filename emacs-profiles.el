35f42caf065cf666a6f0d0f38e8462cef97d2f1f
Chandler DeLoach <deloach.cdd@gmail.com>
2021-11-03 12:43:40 -0600

    make doom the default emacs config

--- emacs-profiles.el
+++ emacs-profiles.el
@@ -1,3 +1,3 @@
-(("default" . ((user-emacs-directory . "~/.potty/emacs/config/vanilla")))
- ("doom"    . ((user-emacs-directory . "~/.potty/emacs/doom-emacs")
-               (env . (("DOOMDIR" . "~/.config/doom"))))))
+(("vanilla" . ((user-emacs-directory . "~/.potty/emacs/config/vanilla")))
+ ("default"    . ((user-emacs-directory . "~/.potty/emacs/doom-emacs")
+                 (env . (("DOOMDIR" . "~/.config/doom"))))))
