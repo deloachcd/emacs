0029c952cb53d275f89b200c9290e99016109db3
Chandler DeLoach <deloach.cdd@gmail.com>
2022-01-02 12:27:45 -0700

    disable flycheck in sh-mode buffers, because I don't need it

--- vanilla/init-layers/shell-layer.el
+++ vanilla/init-layers/shell-layer.el
@@ -11,14 +11,20 @@
   "e" 'eshell
   "s" 'vterm)
 
-(require 'flycheck-layer)
-(setq flycheck-sh-bash-executable "/usr/bin/bash")
-(setq flycheck-sh-zsh-executable "/usr/bin/zsh")
-(defun sh-mode-enable-flycheck-h ()
-  "Enable flycheck for sh buffers"
-  (flycheck-mode)
-  (if (string-match-p (regexp-quote "\.zsh") ".zshrc")
-      (flycheck-select-checker 'sh-zsh)
-    (flycheck-select-checker 'sh-bash)))
+;; NOTE
+;; this works, but I don't think flycheck is really worth it for
+;; just checking simple bash syntax errors at this point. maybe
+;; shellcheck will make it worth it if I ever want to clean up
+;; my scripts
+;;
+;;(require 'flycheck-layer)
+;;(setq flycheck-sh-bash-executable "/usr/bin/bash")
+;;(setq flycheck-sh-zsh-executable "/usr/bin/zsh")
+;;(defun sh-mode-enable-flycheck-h ()
+;;  "Enable flycheck for sh buffers"
+;;  (flycheck-mode)
+;;  (if (string-match-p (regexp-quote "\.zsh") ".zshrc")
+;;      (flycheck-select-checker 'sh-zsh)
+;;    (flycheck-select-checker 'sh-bash)))
 ;; TODO: set up shellcheck and see why this isn't working
 ;;(add-hook 'sh-mode-hook 'sh-mode-enable-flycheck-h)
