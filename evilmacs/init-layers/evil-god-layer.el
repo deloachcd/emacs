4ebf59908b2e1e7e4f670d3408d1395a0a953be0
Chandler DeLoach <deloach.cdd@gmail.com>
2022-01-05 08:30:05 -0700

    use undo-tree as evil-undo-system

--- evilmacs/init-layers/evil-god-layer.el
+++ evilmacs/init-layers/evil-god-layer.el
@@ -1,20 +1,27 @@
 ;; All configuration related to setting up evil-mode and making sure
 ;; it plays nicely with other packages should go here
 
+(use-package undo-tree)
+
 ;; Vim-like bindings
 (use-package evil
+  :after undo-tree
   :init
   (progn
     (setq evil-want-keybinding nil)
     (setq evil-overriding-maps nil
-          evil-intercept-maps nil))
+          evil-intercept-maps nil)
+    (setq evil-undo-system 'undo-tree)
+    (setq evil-local-mode-hook 'turn-on-undo-tree-mode))
   :config (evil-mode t))
 
 ;; Make vim-like bindings play nice everywhere
 (use-package evil-collection
+  :after evil
   :config (evil-collection-init))
 
 (use-package evil-surround
+  :after evil
   :defer
   :hook (prog-mode . evil-surround-mode)
   :hook (text-mode . evil-surround-mode))
