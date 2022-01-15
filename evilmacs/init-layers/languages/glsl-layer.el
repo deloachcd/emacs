20acf3e6f1924a4140697b680745d5b8357649fe
Chandler DeLoach <deloach.cdd@gmail.com>
2022-01-15 14:55:29 -0700

    glsl-viewer-run gets its own binding

--- evilmacs/init-layers/languages/glsl-layer.el
+++ evilmacs/init-layers/languages/glsl-layer.el
@@ -4,3 +4,4 @@
 ;; Load my own extension for launching glslViewer from emacs
 (require 'glsl-viewer)
 (setq glsl-viewer-path "~/.local/bin/glslViewer")
+(general-def 'normal glsl-mode-map "SPC m r" 'glsl-viewer-run)
