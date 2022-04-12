7447b0072049f5dab1184f3d430f134185444ccd
Chandler DeLoach <deloach.cdd@gmail.com>
2022-04-12 15:51:02 -0600

    get rid of emacs-profiles.el

--- emacs-profiles.el
+++
@@ -1,3 +0,0 @@
-(("default" . ((user-emacs-directory . "~/.potty/emacs/config/evilmacs")))
- ("doom"    . ((user-emacs-directory . "~/.potty/emacs/doom-emacs")
-               (env . (("DOOMDIR" . "~/.config/doom"))))))
