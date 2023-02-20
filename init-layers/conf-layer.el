9770ebeab416edd2c04e70e50dbceb94cae0305d
Mildew Man <mildew@poorsoul.ghettobox.xyz>
2023-02-19 17:26:59 -0700

    syntax highlight .yml.j2 as YAML

--- init-layers/conf-layer.el
+++ init-layers/conf-layer.el
@@ -5,6 +5,7 @@
 (require 'yaml-mode)
 (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
 (add-to-list 'auto-mode-alist '("\\.yaml\\'" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.yml\\.j2\\'" . yaml-mode))
 (add-hooks 'yaml-mode-hook prog-mode-minor-modes)
 
 ;; nginx config files
